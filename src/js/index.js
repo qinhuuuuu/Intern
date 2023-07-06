d5ba71ce236fad82d792a3a04fe439860b924f17
Phan Thị Quỳnh Như <20130356@st.hcmuaf.edu.vn>
2023-07-06 23:49:38 +0700

    fix bug menu + thêm focus cho input form thông tin + fix slider khach-hang-noi-gi

--- src/js/index.js
+++ src/js/index.js
@@ -4,27 +4,58 @@ import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.js';
 import "../scss/index.scss";
 
-
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 var main = document.querySelector('main');
 var footer = document.querySelector('footer');
 var dropdownContent = document.getElementsByClassName("dropdown-content");
-var dropdownOpen = false;
 var dropbtn = document.querySelector('.dropbtn');
-dropbtn.onclick = function () {
+
+window.addEventListener("load", function () {
+  checkScreenSize();
+});
+
+// Kiểm tra kích thước màn hình khi thay đổi kích thước trình duyệt
+window.addEventListener("resize", function () {
+  checkScreenSize();
+});
+
+function checkScreenSize() {
+  const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if (screenWidth > 1280) {
+    cancelMenu();
+  } else {
+    dropbtn.onclick = openMenu;
+
+  }
+}
+var isOpenMenu = false;
+function cancelMenu() {
+  if (dropdownContent[0].classList.contains("show")) {
+    for (let i = 0; i < dropdownContent.length; i++) {
+      dropdownContent[i].classList.remove("show");
+    }
+    if (main.classList.contains('hidden')) {
+      dropbtn.classList.remove('text-primary-1');
+      dropbtn.classList.add('text-white');
+      main.classList.remove('hidden');
+      footer.classList.remove('hidden');
+    }
+  }
+}
+function openMenu() {
   for (let i = 0; i < dropdownContent.length; i++) {
     dropdownContent[i].classList.toggle("show");
-    dropdownOpen = dropdownContent[0].classList.contains("show");
   }
-  // Ẩn hiện main và footer
-  main.classList.toggle('hidden', dropdownOpen);
-  footer.classList.toggle('hidden', dropdownOpen);
-  // Đổi màu dropbtn
-  dropbtn.classList.toggle("text-primary-1", dropdownOpen);
-  dropbtn.classList.toggle("text-white", !dropdownOpen);
+  isOpenMenu = dropdownContent[0].classList.contains('show');
+
+  dropbtn.classList.toggle('text-primary-1', isOpenMenu);
+  dropbtn.classList.toggle('text-white', !isOpenMenu);
+  main.classList.toggle('hidden', isOpenMenu);
+  footer.classList.toggle('hidden', isOpenMenu);
 }
+
 $(document).ready(function () {
   $('.slider-kinh-nghiem-lam-viec').slick({
     infinity: true,
@@ -50,7 +81,12 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
   $('.slider-khach-hang-noi-gi').slick({
-    arrows: false,
+    infinity: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.twi-19-arrow-back-fill'),
+    nextArrow: $('.twi-19-arrow-forward-fill'),
     dots: true,
     mobileFirst: true,
     customPaging: function (slider, i) {
@@ -61,9 +97,6 @@ $(document).ready(function () {
         breakpoint: 767,
         settings: {
           dots: false,
-          arrows: true,
-          prevArrow: $('.twi-19-arrow-back-fill'),
-          nextArrow: $('.twi-19-arrow-forward-fill')
         }
       }
     ]
